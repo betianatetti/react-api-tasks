0203cd6b5f07becffb81df1de8496573ed823de9
juan9728 <juanpromanzio@gmail.com>
2019-05-08 23:13:46 -0300

    add error description

--- src/App.js
+++ src/App.js
@@ -24,5 +24,13 @@ class App extends Component {
   }
 }
 
+/*
+Error appear: 
+Unchecked runtime.lastError: The message port closed before a response was received.
+localhost/:1 Access to fetch at 'http://localhost:3000/tasks' from origin 'http://localhost:3001' has been blocked by CORS policy: 
+  No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' 
+  to fetch the resource with CORS disabled.
+*/
+
 
 export default App;
